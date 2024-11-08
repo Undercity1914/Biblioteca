6a85b4370f4333e01726d781f0cff10c37bf3a45
Marcos Vinicius Otaviano Gonçalves <137358236+Undercity1914@users.noreply.github.com>
2024-11-08 20:07:23 -0300

    version 1.0

---
+++ src/view/cadastro-de-livros/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/header'
+import Header from './components/header/index.js';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/view/cadastro-de-livros/src/components/header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.css';
+
+const Header = () => {
+    return (
+        <nav class="header">
+            <div class="header-title">
+                <p>Minha</p>
+                <p>Bilioteca</p>
+                <p>Pessoal</p>
+            </div>
+            <div class="header-bar">
+                
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
