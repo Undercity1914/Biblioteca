3d6f0fe8f440409255eff64fd4f1df85a615fea4
Marcos Vinicius Otaviano Gonçalves <137358236+Undercity1914@users.noreply.github.com>
2024-11-28 15:19:10 -0300

    version 1.0.1

--- src/view/cadastro-de-livros/src/components/header/index.js
+++ src/view/cadastro-de-livros/src/components/header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './style.css';
+import search from "../../assets/search.png"
 
 const Header = () => {
     return (
@@ -10,7 +11,28 @@ const Header = () => {
                 <p>Pessoal</p>
             </div>
             <div class="header-bar">
-                
+                <p class="header-text-bar" style={{marginLeft: "115px"}}>
+                    Procurar
+                </p>
+                <img src={search} alt="Search" style={{width: "15px", marginLeft: "10px"}}/>
+                <input
+                    type="text"
+                    value={''}
+                    onChange={''}
+                    class="header-search-bar"
+                />
+                <p class="header-text-bar" style={{marginLeft: "50px"}}>
+                    Adicionar
+                </p>
+                <p class="header-text-bar" style={{marginLeft: "50px"}}>
+                    Remover
+                </p>
+                <p class="header-text-bar" style={{marginLeft: "50px"}}>
+                    Editar
+                </p>
+                <p class="header-text-bar" style={{marginLeft: "50px"}}>
+                    Outros
+                </p>
             </div>
         </nav>
     );
